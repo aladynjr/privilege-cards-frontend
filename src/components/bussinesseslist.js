fe891b5370f834e6fd86c8aed2409759f988b59a
aladynjr <aladynjr@gmail.com>
2022-09-06 08:34:31 +0100

    add more details modal

--- src/components/bussinesseslist.js
+++ src/components/bussinesseslist.js
@@ -1,5 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import Button from '@mui/material/Button';
+import Modal from './modal';
+import { MdCardMembership } from 'react-icons/md';
+import { MdPhoneInTalk } from 'react-icons/md';
+import { AiFillStar } from 'react-icons/ai';
+import { HiGift } from 'react-icons/hi';
+import { FaMapMarkedAlt } from 'react-icons/fa';
+import { ImClock2 } from 'react-icons/im';
+import { RiCustomerService2Fill } from 'react-icons/ri';
+import ImageGallery from 'react-image-gallery';
+
+
+
+
+import Chip from '@mui/material/Chip';
+
+import Divider from '@mui/material/Divider';
 
 function BussinessesList({ bussinesses, setBussinesses, favouriteBussinesses, setFavouriteBussinesses, userID, userName, cardAvailable, loggedInButtons = false }) {
 
@@ -65,63 +81,146 @@ function BussinessesList({ bussinesses, setBussinesses, favouriteBussinesses, se
         }
     }
 
+    useEffect(() => {
+        setSelectedBussiness(bussinesses[0])
+    }, [bussinesses])
 
+    const [bussinessDetailsModal, setBussinessDetailsModal] = useState(false)
+
+    console.log(selectedBussiness?.bussiness_cover_image_urls)
+
+    var coverImages = selectedBussiness?.bussiness_cover_image_urls?.map((image) => {
+        return {
+            original: image,
+            // thumbnail: image,
+        }
+
+    })
     return (
         <div>
-            <p>All Bussinesses</p>
-            <div style={{display:'flex', justifyContent:'space-evenly', flexWrap:'wrap'}}>
-            {bussinesses && bussinesses.map((bussiness, i) => {
-                return (
-                    <div key={i}>
-                        <div className="outer-div">
-                            <div className="inner-div">
-                                <div className="front">
+            <h1 style={{ opacity: '0.9' }} >AVAILABLE OFFERS</h1>
+            <div style={{ display: 'flex', justifyContent: 'space-evenly', flexWrap: 'wrap' }}>
+                {bussinesses && bussinesses.map((bussiness, i) => {
+                    return (
+                        <div key={i}>
+                            <div className="outer-div">
+                                <div className="inner-div">
+                                    <div className="front">
 
-                                    <img className="front__bkg-photo" style={{ cursor: 'pointer' }} src={bussiness?.bussiness_cover_image_url} />
+                                        <img className="front__bkg-photo" style={{ cursor: 'pointer' }} src={bussiness?.bussiness_cover_image_url} />
 
-                                    <img className="front__face-photo" style={{ cursor: 'pointer' }} src={bussiness?.bussiness_profile_image_url} />
+                                        <img className="front__face-photo" style={{ cursor: 'pointer' }} src={bussiness?.bussiness_profile_image_url} />
 
-                                    <div className="front__text">
-                                        <h3 className="front__text-header">{bussiness?.bussiness_name}</h3>
-                                        {/* <p className="front__text-para"><i className="fas fa-map-marker-alt front-icons"></i>{bussiness?.bussiness_area}</p> */}
+                                        <div className="front__text">
+                                            <h3 className="front__text-header">{bussiness?.bussiness_name}</h3>
+                                            <p className="front__text-para"><i className="front-icons"></i>{bussiness?.bussiness_discount}% OFF</p>
 
-                                        <Button variant="contained" style={{background: 'dodgerblue', width:'70%'}} onClick={() => { AddFavouriteBussiness(userID, bussiness) }} >VIEW</Button>
+                                            <Button variant="contained" style={{ background: 'dodgerblue', width: '70%', fontSize: '15px', fontWeight: '100', fontFamily: 'Crimson Pro', marginTop: '10px' }} onClick={() => { setSelectedBussiness(bussiness); setBussinessDetailsModal(true) }} >VIEW</Button>
 
-                                        {/* <span className="front__text-hover">
+                                            <div>
+                                                <Chip className="bussinesscardtag" label={bussiness?.bussiness_cuisine} />
+
+                                            </div>
+                                            {/* <span className="front__text-hover">
                                             {loggedInButtons && <div>
                                                 <Button variant="contained" color="error" onClick={() => { AddFavouriteBussiness(userID, bussiness) }} >Favourite</Button>
                                                 <Button disabled={!cardAvailable} variant="contained" color="success" onClick={() => { AddPrivilegeCard(bussiness?.bussiness_id, bussiness?.bussiness_discount); setSelectedBussiness(bussiness); setShowCard(true); }} >Show Card</Button>
                                             </div>}
 
                                         </span> */}
+
+                                        </div>
                                     </div>
+
+
                                 </div>
-
-
                             </div>
+
+
+
                         </div>
-
-
-
-                    </div>
-                )
-            })}
+                    )
+                })}
             </div>
 
 
 
+<div style={{zIndex:'2001', position:'absolute'}}>
+{showCard && <Modal
+                fullscreen={true}
+                onClick={() => { setShowCard(false) }}
+                titletext=""
+                titlecolor='whitesmoke'
+                bodytext={
+                    <div className="bussinesscard">
+                    <p>Name : {userName}</p>
+                    <p>Bussiness : {selectedBussiness?.bussiness_name}</p>
+                    <p>discount : {selectedBussiness?.bussiness_discount}%</p>
+                    <p>date : {new Date().toLocaleString() + ""}</p>
+                    <p>bussiness code : {String(selectedBussiness?.bussiness_id).padStart(4, '0')} </p>
+                    <p>unique ref code : {Math.floor(1000 + Math.random() * 9000)} </p>
+                    <img width={'70px'} style={{ cursor: 'pointer' }} src={'https://avatars.dicebear.com/api/identicon/' + String(selectedBussiness?.bussiness_id).padStart(4, '0') + '.svg?mood[]=happy'} />
+                    <span class="holo"></span>
+
+                </div>
+                    
+                }
 
 
-            {showCard && <div className="bussinesscard">
-                <p>Name : {userName}</p>
-                <p>Bussiness : {selectedBussiness?.bussiness_name}</p>
-                <p>discount : {selectedBussiness?.bussiness_discount}%</p>
-                <p>date : {new Date().toLocaleString() + ""}</p>
-                <p>bussiness code : {String(selectedBussiness?.bussiness_id).padStart(4, '0')} </p>
-                <p>unique ref code : {Math.floor(1000 + Math.random() * 9000)} </p>
-                <img width={'70px'} style={{ cursor: 'pointer' }} src={'https://avatars.dicebear.com/api/identicon/' + String(selectedBussiness?.bussiness_id).padStart(4, '0') + '.svg?mood[]=happy'} />
+            />}
+</div>
 
-            </div>}
+
+
+            {bussinessDetailsModal && <Modal
+                fullscreen={true}
+                onClick={() => { setBussinessDetailsModal(false) }}
+                titletext=""
+                titlecolor='whitesmoke'
+                bodytext={
+                    <div style={{ width: 'min-content' }}>
+                        <div className="front__bkg-photo" style={{ width: '100vw', objectFit: 'cover', height: 'fit-content' }} >   <ImageGallery items={coverImages || []} showThumbnails={false} autoPlay={true} showFullscreenButton={false} /></div>
+
+                        {/* <img className="front__bkg-photo" style={{width: '100vw', objectFit: 'cover', height: '700px' }} src={selectedBussiness?.bussiness_cover_image_url} /> */}
+
+                        <img className="front__face-photo" style={{ cursor: 'pointer', top: '-34px', width: '100px', height: '100px' }} src={selectedBussiness?.bussiness_profile_image_url} />
+                        <div className="front__text">
+                            <h3 className="front__text-header">{selectedBussiness?.bussiness_name}</h3>
+                            <Button
+                                disabled={!cardAvailable}
+                                endIcon={<MdCardMembership />}
+                                style={{ fontFamily: 'Crimson Pro', padding: '10px 50px', fontSize: '17px', background: 'limegreen' }}
+                                variant="contained" color="success"
+                                onClick={() => {/* AddPrivilegeCard(selectedBussiness?.bussiness_id, selectedBussiness?.bussiness_discount);*/ setShowCard(true); }} >Show Card</Button>
+
+                        </div>
+                        <div style={{ width: '50%', minWidth: '280px', margin: 'auto' }}>
+                            <Divider><h3><AiFillStar style={{ marginBottom: '-5px', opacity: '0.8' }} /> About </h3></Divider>
+                            <p className='bussinessdetailstext' >{selectedBussiness?.bussiness_description}</p>
+
+                            <Divider>  <h3 className="front__text-para" style={{ fontSize: '25px', fontWeight: '700px' }} ><HiGift style={{ marginBottom: '-5px', opacity: '0.8' }} /> {selectedBussiness?.bussiness_discount}% OFF</h3></Divider>
+                            <p className='bussinessdetailstext'>{selectedBussiness?.bussiness_discountdetails}</p>
+                            {loggedInButtons && <div>
+                                {/* <Button variant="contained" color="error" onClick={() => { AddFavouriteBussiness(userID, selectedBussiness) }} >Favourite</Button> */}
+                            </div>}
+
+
+                            <Divider><h3 className="front__text-header"> <FaMapMarkedAlt style={{ marginBottom: '-5px', opacity: '0.8' }} /> Location</h3> </Divider>
+
+                            <p className='bussinessdetailstext'>{selectedBussiness?.bussiness_locationdetails}</p>
+                            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>  <a href={selectedBussiness?.bussiness_directions_url} target={'_blank'} style={{ padding: '0px 10px', fontWeight: '900' }}  >DIRECTIONS  </a> <img width={'25px'} src={'https://cdn-icons-png.flaticon.com/512/355/355980.png'} /></div>
+                            <Divider> <h3 className="front__text-header"> <ImClock2 style={{ marginBottom: '-5px', opacity: '0.8' }} /> Trading Hours</h3></Divider>
+                            <p className='bussinessdetailstext'>{selectedBussiness?.bussiness_tradinghours}</p>
+
+                            <Divider> <h3 className="front__text-header"><RiCustomerService2Fill style={{ marginBottom: '-5px', marginRight: '7px', opacity: '0.8' }} />Contact</h3></Divider>
+                            <a className='bussinessdetailstext' href={"tel:" + selectedBussiness?.bussiness_phonenumber}> <p><MdPhoneInTalk style={{ marginBottom: '-5px' }} />{selectedBussiness?.bussiness_phonenumber} </p></a>
+
+                        </div>
+                        <div style={{ marginBottom: '200px' }} ></div>
+                    </div>}
+
+
+            />}
 
         </div>
     )
---
+++ src/components/modal.js
@@ -0,0 +1,69 @@
+import React, { useEffect } from 'react'
+import clsx from 'clsx';
+
+function Modal({onClick,
+                      titletext,
+                      titlecolor,
+                      bodytext, 
+                      titlebgcolor, 
+                      buttontext, 
+                      buttonbgcolor, 
+                      notice, 
+                      points, 
+                      custommessage,
+                      buttontext2,
+                      buttonbgcolor2,
+                      onClick2,
+                      fullscreen=false, 
+                      backgroundColor,
+                      buttonClassName,
+                      buttonContainerClassName,
+                      enteranimation=true,
+                    }) {
+  
+    //click enter to run onclick
+    useEffect(() => {
+      const listener = event => {
+        if ((event.code === "Enter" || event.code === "NumpadEnter")) {
+          
+          event.preventDefault();
+          document.body.style.overflow = 'unset';
+          onClick();
+        }
+      };
+      document.addEventListener("keydown", listener);
+      return () => {
+        document.removeEventListener("keydown", listener);
+      };
+    }, []);
+useEffect(()=>{
+    if (typeof window != 'undefined' && window.document) {
+        document.body.style.overflow = 'hidden';
+    }
+},[])
+  return (
+    <div>
+        
+        <div className={clsx("modal  ",enteranimation && 'animate slide', fullscreen && 'fullscreenmodal')} style={{ zIndex: '1000', marginTop: '0px' }} lang='ar'>
+                    <div  style={{ background: 'transparent', zIndex: '10', opacity: '0' }} className="overlay"></div>
+                    <div className={clsx("modal-content simplecard ", fullscreen && 'fullscreenmodal')} style={{ zIndex: '10',boxShadow: '1px 0.5px 10px 1px lightslategrey', padding:'0px 10px', background: backgroundColor }}>
+                        {titletext && <h1 style={{ marginBottom: '0', backgroundColor: titlebgcolor,color:titlecolor, borderRadius: '20px', padding: '0px 20px',height:' fit-content', zIndex:'5' }}>{titletext}</h1>}
+                        <div>{bodytext}</div>
+                        {points && <h3 style={{direction:'rtl', opacity:'0.4'}}>+ {points} نقطة</h3>}
+                        {custommessage}
+                        {buttontext &&<div className={buttonContainerClassName} /*style={{position: 'absolute', bottom: '50px'}}*/> <button className={'accept   '+buttonClassName} style={{backgroundColor: buttonbgcolor}} onClick={onClick} lang='ar'>{buttontext}</button>
+                        <div style={{opacity:'0.5', fontSize:'13px', marginTop:'0px'}} className='hideonmobile'>Touche d'entrée</div>
+                        </div>}
+                        
+
+                        {buttontext2 && <button className='accept ' style={{backgroundColor: buttonbgcolor2}} onClick={onClick2} lang='ar'>{buttontext2}</button>}
+
+                         <i style={{ color: 'grey' }}>{notice}</i> 
+
+                    </div>
+                </div>
+    </div>
+  )
+}
+
+export default Modal
